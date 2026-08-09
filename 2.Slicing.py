c64f2deecf32d1093a4c327d30c2565fc2c3b186
hasini-1107 <hasinipenumesta@gmail.com>
2026-08-09 16:55:49 +0530

    initial commit

--- 2.Slicing.py
+++ 2.Slicing.py
@@ -11,3 +11,4 @@ print(array[::-1])  # reveresed
 print(array[:, -1])  # all rows and last column
 print(array[:, 1:3])  # print all rows and 1,2 columns
 print(array[:, ::2])  # similar to array[0:4:2] but columns
+print(array[1:3, 1:3])  # print rows 1,2 and columns 1,2
